69c5a41704304e3c90cb450344ea6e426ea43a5e
BDFarrey <bethanyfarrey@yahoo.com>
2017-03-02 17:13:13 -0500

    added has many to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-  has_many: challenges
+  has_many :challenges
+  has_and_belongs_to_many :users,
+              class_name: "User",
+              join_table: :challenges, 
+              foreign_key: :user_id,
+              association_foreign_key: :challenger_user_id
 end
