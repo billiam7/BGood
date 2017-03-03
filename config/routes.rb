6cd066dc74769d66431272d67e3711b0c7666d1a
BDFarrey <bethanyfarrey@yahoo.com>
2017-03-03 17:47:28 -0500

    added auth0 login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
+
+  get "/auth/oauth2/callback" => "auth0#callback"
+
+  get "/auth/failure" => "auth0#failure"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 
   resource :users
   resources :controllers
