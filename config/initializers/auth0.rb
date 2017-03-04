0ad0a71ddcd633298e0f18a560e22fa0f5797d63
BDFarrey <bethanyfarrey@yahoo.com>
2017-03-03 20:26:43 -0500

    minor changes

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    'kZb2T8rUqTB82PgRyv20Tj960RNiJQPT',
-    'BgrsaZHVCO_pjxP0aYE3qotaBHKpCgmu82EJfkzh9VdHd3y25eNQSIq63d0UF5ld',
-    'bgood.auth0.com',
+
     callback_path: "/auth/oauth2/callback"
   )
 end
