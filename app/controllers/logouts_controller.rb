4144b411f566df97fe61da6a0662d508b67c282c
BDFarrey <bethanyfarrey@yahoo.com>
2017-03-06 15:06:32 -0500

    added phonenumber column to users

--- app/controllers/logouts_controller.rb
+++ app/controllers/logouts_controller.rb
@@ -1,9 +1,9 @@
 module LogoutHelper
   def logout_url
-    creds = { client_id: Rails.application.secrets.auth0_client_id,
-    client_secret: Rails.application.secrets.auth0_client_secret,
+    creds = { client_id: ENV['auth0_bgood_key'],
+    client_secret: ENV['auth0_bgood_secret'],
     api_version: 1,
-    domain: Rails.application.secrets.auth0_domain}
+    domain: ENV['auth0_domain']}
     auth0_client = Auth0Client.new(creds)
     auth0_client.logout_url(root_url)
   end
