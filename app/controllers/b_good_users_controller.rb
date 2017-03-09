71b3ef03834f77e440fd2cc705b641ee7a070fc5
BDFarrey <bethanyfarrey@yahoo.com>
2017-03-08 19:22:43 -0500

    added facebook share button

--- app/controllers/b_good_users_controller.rb
+++ app/controllers/b_good_users_controller.rb
@@ -14,12 +14,16 @@ class BGoodUsersController < ApplicationController
   end
 
   def create
-    @b_good_user = BGoodUser.create user_params
+    @b_good_user = BGoodUser.new user_params
+    @b_good_user.email = session[:userinfo]['info']['email']
+    @b_good_user.auth0_id = session[:userinfo]['uid']
+    @b_good_user.referer = session[:referer] if session[:referer]
+    @b_good_user.save!
     redirect_to dashboard_path, notice: "Thank you for completing your profile."
   end
 
   private
   def user_params
-    params.require(:b_good_user).permit(:name, :email, :phone_number, :auth0_id)
+    params.require(:b_good_user).permit(:name)
   end
 end
