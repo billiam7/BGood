bfa20fe85e31f4806ce08887d978c051d26b33bd
Bill Lapko <wlapko@gmail.com>
2017-03-08 20:07:03 -0500

    added modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,3 +44,9 @@ $('.modal').modal({
      complete: function() { alert('Closed'); } // Callback for Modal close
    }
  );
+ $(document).ready(function(){
+   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+   $('.modal').modal();
+   $('#modal1').modal('open');
+   $('#modal1').modal('close');
+ });
