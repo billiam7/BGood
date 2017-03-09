b9743d166232b838379a1b2cf9162c58c8ba1bcc
Bill Lapko <wlapko@gmail.com>
2017-03-09 18:04:39 -0500

    styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,39 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-// $('.button-collapse').sideNav({
-//      menuWidth: 300, // Default is 300
-//      edge: 'right', // Choose the horizontal origin
-//      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-//      draggable: true // Choose whether you can drag to open on touch screens
-//    }
-//  );
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-$('.modal').modal({
-     dismissible: true, // Modal can be dismissed by clicking outside of the modal
-     opacity: .5, // Opacity of modal background
-     inDuration: 300, // Transition in duration
-     outDuration: 200, // Transition out duration
-     startingTop: '4%', // Starting top style attribute
-     endingTop: '10%', // Ending top style attribute
-     ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-       alert("Ready");
-       console.log(modal, trigger);
-     },
-     complete: function() { alert('Closed'); } // Callback for Modal close
-   }
- );
- $(document).ready(function(){
-   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-   $('.modal').modal();
-   $('#modal1').modal('open');
-   $('#modal1').modal('close');
- });
--- app/assets/javascripts/public_pages.js
+++ app/assets/javascripts/public_pages.js
@@ -1,1 +1,17 @@
+// (function($){
+//   $(function(){
+//
+//     $('.button-collapse').sideNav();
+//     $('.parallax').parallax();
+//
+//   }); // end of document ready
+// })(jQuery); // end of jQuery name space
 
+
+// $('.button-collapse').sideNav({
+//      menuWidth: 300, // Default is 300
+//      edge: 'right', // Choose the horizontal origin
+//      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+//      draggable: true // Choose whether you can drag to open on touch screens
+//    }
+//  );
