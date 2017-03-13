bb7b71436230bd39de6fbad58846ce841947fd9e
BDFarrey <bethanyfarrey@yahoo.com>
2017-03-13 13:28:59 -0400

    removed from migration 20170305021855 adding column user_id

--- db/migrate/20170305021855_challenged.rb
+++ db/migrate/20170305021855_challenged.rb
@@ -1,7 +1,7 @@
 class Challenged < ActiveRecord::Migration[5.0]
   def change
     add_column :challenges, :challenger_user_id, :integer
-    add_column :challenges, :user_id, :integer
+    # add_column :challenges, :user_id, :integer
   end
   #   add_index(:challenges, [:user_id, :challenger_user_id])
   #   add_index(:challenges, [:challenger_user_id, :user_id])
